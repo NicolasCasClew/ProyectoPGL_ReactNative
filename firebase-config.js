82544b01c883503f69c650caf3fe58c206ab6b59
NicolasCasClew <nicolascasclew@gmail.com>
2023-02-07 03:12:56 +0000

    Apis escondidas, puedo publicar

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,18 @@
 // Import the functions you need from the SDKs you need
 import * as firebase from "firebase";
+import * as data from './keys.json';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
  const firebaseConfig = {
-  apiKey: "AIzaSyB_F-Crly_Jt9PhJ3aLuvtJzetAVFSokaE",
-  authDomain: "react-pgl.firebaseapp.com",
-  projectId: "react-pgl",
-  storageBucket: "react-pgl.appspot.com",
-  messagingSenderId: "747048326859",
-  appId: "1:747048326859:web:d3cb276b225acdc4106d4e"
+  apiKey: data.apiLlave,
+  authDomain: data.authDominio,
+  projectId: data.projectIdentificacion,
+  storageBucket: data.storageCubo,
+  messagingSenderId: data.messagingEnviadorId,
+  appId: data.appIdentificador,
 };
 
 let app;
